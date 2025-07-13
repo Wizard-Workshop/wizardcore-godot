369e7ffe8196b107b8a989aed7d11eb8434d5bb9
ww-jonas <jonas@wizardworkshop.studio>
2025-07-13 17:03:41 -0600

    chore: flatten folder structure

--- addons/wizardcore/auth/guest_auth.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-signal authenticated(uid)
-var uid:String
-func login_guest():
-    uid = "guest-%06d" % randi()
-    emit_signal("authenticated", uid)
--- addons/wizardcore/wizard_autoload.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-const Auth = preload("res://addons/wizardcore/auth/guest_auth.gd").new()
-func _enter_tree(): add_child(Auth)
