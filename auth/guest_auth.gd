10da9d2e145e4b9efc4bd695e000f661fff689b6
ww-jonas <jonas@wizardworkshop.studio>
2025-07-13 18:08:38 -0600

    fix: standardize indentation in guest_auth, wizard_autoload, and wizard_plugin scripts

--- auth/guest_auth.gd
+++ auth/guest_auth.gd
@@ -2,5 +2,5 @@ extends Node
 signal authenticated(uid)
 var uid:String
 func login_guest():
-    uid = "guest-%06d" % randi()
-    emit_signal("authenticated", uid)
+	uid = "guest-%06d" % randi()
+	emit_signal("authenticated", uid)
--- wizard_plugin.gd
+++ wizard_plugin.gd
@@ -5,9 +5,9 @@ const AUTOLOAD_NAME := "WizardCore"
 const AUTOLOAD_PATH := "res://addons/wizardcore/wizard_autoload.gd"
 
 func _enter_tree():
-    if not Engine.has_singleton(AUTOLOAD_NAME):
-        add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
+	if not Engine.has_singleton(AUTOLOAD_NAME):
+		add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_PATH)
 
 func _exit_tree():
-    if Engine.has_singleton(AUTOLOAD_NAME):
-        remove_autoload_singleton(AUTOLOAD_NAME)
+	if Engine.has_singleton(AUTOLOAD_NAME):
+		remove_autoload_singleton(AUTOLOAD_NAME)
